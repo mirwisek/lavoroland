481fe83069dfbf8fae7cc1e22cfad230aba33115
SYusupov <sayor1999@gmail.com>
2023-12-21 13:04:16 +0100

    Cleaning up some stuff

--- script.js
+++ script.js
@@ -395,15 +395,16 @@ height = 400 - margin.top - margin.bottom;
 async function plot_health(checkedCountries) {
     try {
         // Load CSV files
-        const [data] = await Promise.all([
-            d3.csv("./data/insurance_proportion.csv")
+        const [insurance_data, indexes] = await Promise.all([
+            d3.csv("./data/insurance_proportion.csv"),
+            d3.csv("./data/health_indexes.csv")
         ]);
 
         // console.log("data", data);
 
         // console.log("checkedCountries", checkedCountries);
 
-        var filteredData = data.filter(function(d) 
+        var filteredData = insurance_data.filter(function(d) 
         {
             if( (checkedCountries.findIndex(element => element.includes(d["name"]))) != -1)
             {
@@ -412,7 +413,7 @@ async function plot_health(checkedCountries) {
         });
         // console.log("filteredData", filteredData);
 
-        var subgroups = data.columns.slice(1);
+        var subgroups = insurance_data.columns.slice(1);
         
         // console.log("subgroups", subgroups);
         // console.log("filteredData", filteredData);
@@ -421,7 +422,7 @@ async function plot_health(checkedCountries) {
         createMarimekkoChart(filteredData, subgroups)
 
         // Get unique country names
-        var uniqueNames = d3.map(data, function(d) {
+        var uniqueNames = d3.map(insurance_data, function(d) {
             return d.name;
         });
 
@@ -442,6 +443,17 @@ async function plot_health(checkedCountries) {
             el2.value = opt;
             select2.appendChild(el2);
         }
+
+        // // // FOR PARALLEL COORDINATES
+        // var filteredIndexes = insurance_data.filter(function(d) 
+        // {
+        //     if( (checkedCountries.findIndex(element => element.includes(d["name"]))) != -1)
+        //     {
+        //         return d;
+        //     }
+        // });
+        // var subgroups = indexes.columns.slice(1);
+        // createParallelCoordinates(filteredIndexes, subgroups);
     } catch (error) {
         console.error("Error loading CSV data: ", error);
     }
@@ -589,4 +601,72 @@ const createMarimekkoChart = (filteredData, subgroups) => {
             .attr("y", function(d) { return y(d[1]); })
             .attr("height", function(d) { return y(d[0]) - y(d[1]); })
             .attr("width",x.bandwidth())
+}
+
+const createParallelCoordinates = (data, dimensions) => {
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 30, right: 10, bottom: 10, left: 0},
+    width = 500 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#stacked-bar-chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
+    // dimensions = d3.keys(data[0]).filter(function(d) { return d != "Species" })
+
+    // For each dimension, I build a linear scale. I store all in a y object
+    var y = {}
+    for (i in dimensions) {
+    name = dimensions[i]
+    y[name] = d3.scaleLinear()
+        .domain( d3.extent(data, function(d) { return +d[name]; }) )
+        .range([height, 0])
+    }
+
+    // Build the X scale -> it find the best position for each Y axis
+    x = d3.scalePoint()
+    .range([0, width])
+    .padding(1)
+    .domain(dimensions);
+
+    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+    function path(d) {
+        return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+    }
+
+    // Draw the lines
+    svg
+    .selectAll("myPath")
+    .data(data)
+    .enter().append("path")
+    .attr("d",  path)
+    .style("fill", "none")
+    .style("stroke", "#69b3a2")
+    .style("opacity", 0.5)
+
+    // Draw the axis:
+    svg.selectAll("myAxis")
+    // For each dimension of the dataset I add a 'g' element:
+    .data(dimensions).enter()
+    .append("g")
+    // I translate this element to its right position on the x axis
+    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+    // And I build the axis with the call function
+    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+    // Add axis title
+    .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -9)
+        .text(function(d) { return d; })
+        .style("fill", "black")
+
+
 }
